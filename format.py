ab5fb33282e1e7648bc81412f183dc18944b1e41
Tyler Veness <calcmogul@gmail.com>
2019-02-02 10:27:56 -0800

    Remove redundant formatter call
    Change-Id: Ia46738b5b055c8c459b17b9a24f200065b0dda4e

--- format.py
+++ format.py
@@ -8,6 +8,5 @@ files = [
     os.path.join(dp, f) for dp, dn, fn in os.walk(".") for f in fn if f.endswith(".py")
 ]
 
-subprocess.run([sys.executable, "-m", "black", "-q", "format.py"])
 for f in files:
     subprocess.run([sys.executable, "-m", "black", "-q", f])
