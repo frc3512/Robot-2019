1a9f7de8870fa0c322231880142faf4fdcc009b7
Tyler Veness <calcmogul@gmail.com>
2020-01-08 20:44:58 -0800

    Update state-space v2 from upstream
    Change-Id: I376d2f5b5e55934695c79471ce14f8ed7da04a14

--- thirdparty/cpp/frc/system/plant/DrivetrainVelocitySystem.cpp
+++ thirdparty/cpp/frc/system/plant/DrivetrainVelocitySystem.cpp
@@ -1,17 +1,17 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "frc/system/plant/DrivetrainRefTrackerSystem.h"
+#include "frc/system/plant/DrivetrainVelocitySystem.h"
 
 #include <cmath>
 
 namespace frc {
 
-LinearSystem<2, 2, 2> DrivetrainRefTrackerSystem(
+LinearSystem<2, 2, 2> DrivetrainVelocitySystem(
     DCMotor motor, units::kilogram_t m, units::meter_t r, units::meter_t rb,
     double G, units::kilogram_square_meter_t J) {
   auto C1 = -std::pow(G, 2) * motor.Kt /
