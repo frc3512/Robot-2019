a66eacd917f01d882b844c8282db4c7889091b66
Tyler Veness <calcmogul@gmail.com>
2020-01-19 09:56:40 -0800

    Move moment of inertia unit definition into units.h

--- thirdparty/include/frc/system/plant/DCMotor.h
+++ thirdparty/include/frc/system/plant/DCMotor.h
@@ -9,12 +9,6 @@
 
 #include <units.h>
 
-namespace units {
-UNIT_ADD(moment_of_inertia, kilogram_square_meter, kilogram_square_meters,
-         kg_sq_m, compound_unit<kilogram, square_meter>)
-using namespace moment_of_inertia;
-}  // namespace units
-
 namespace frc {
 
 /**
