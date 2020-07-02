a7bef60a9ec327346f183b9d8367dd2fdc87ac23
Tyler Veness <calcmogul@gmail.com>
2020-07-02 16:35:15 -0700

    Remove DS Display code
    The DS Display UI wasn't used, and we didn't need autonomous selection
    since we just did teleop during sandstorm. Pubsub uses Packet though, so
    that was kept.

--- src/main/include/autonselector/Packet.hpp
+++ src/main/include/autonselector/Packet.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2019 FRC Team 3512. All Rights Reserved.
+// Copyright (c) 2020 FRC Team 3512. All Rights Reserved.
 
 #pragma once
 
@@ -47,6 +47,8 @@ public:
     Packet& operator>>(uint16_t& data);
     Packet& operator>>(int32_t& data);
     Packet& operator>>(uint32_t& data);
+    Packet& operator>>(int64_t& data);
+    Packet& operator>>(uint64_t& data);
     Packet& operator>>(float& data);
     Packet& operator>>(double& data);
     Packet& operator>>(std::string& data);
@@ -57,6 +59,8 @@ public:
     Packet& operator<<(uint16_t data);
     Packet& operator<<(int32_t data);
     Packet& operator<<(uint32_t data);
+    Packet& operator<<(int64_t data);
+    Packet& operator<<(uint64_t data);
     Packet& operator<<(float data);
     Packet& operator<<(double data);
     Packet& operator<<(const std::string& data);
